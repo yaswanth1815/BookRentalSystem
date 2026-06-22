8c4383b764dac142476fc2f741bb69578b519596
Yaswanth Reddy <165630157+yaswanth1815@users.noreply.github.com>
2026-06-22 23:35:56 +0530

    Take Some Break

--- src/main/java/com/java/vintageBooks/controller/CartController.java
+++ src/main/java/com/java/vintageBooks/controller/CartController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -41,11 +43,12 @@ public class CartController {
         return "redirect:/buyingbook";
     }
 
-    @PostMapping("/buyerscart")
-    public String showInCart(HttpSession session){
+    @GetMapping("/buyerscartshow")
+    public String showInCart(HttpSession session,Model model){
         Buyer buyer=(Buyer) session.getAttribute("buyerobject");
         List<BuyerCart> buyerCart=buyerCartService.getCartByUsername(buyer.getUsername());
-        b
+        model.addAttribute("buyerCartList",buyerCart);
+        model.addAttribute("count",buyerCart.size());
         return "buyercart";
     }
 }
